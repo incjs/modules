102c821446c989fcffe4222d366446ba6ebcb810
lifesinger <lifesinger@gmail.com>
2011-10-18 21:00:57 +0800

    rebuild registry

--- registry.js
+++ registry.js
@@ -1,27 +1,4 @@
 define({
-  "jquery": {
-    "name": "jQuery",
-    "description": "A new kind of JavaScript Library.",
-    "author": "John Resig",
-    "url": "http://jquery.com/",
-    "keywords": [ "dom", "event", "library" ],
-    "version": "1.6.4",
-    "src": "http://code.jquery.com/jquery-1.6.4.js",
-    "min": "http://code.jquery.com/jquery-1.6.4.min.js",
-    "gzipped": 32090
-  },
-  "es5-safe": {
-    "package": "https://raw.github.com/seajs/dew/master/src/es5-safe/package.json",
-    "name": "es5-safe",
-    "description": "Provides compatibility shims so that legacy JavaScript engines behave as closely as possible to ES5.",
-    "url": "https://github.com/seajs/dew/tree/master/src/es5-safe",
-    "keywords": [ "util" ],
-    "author": "Frank Wang <lifesinger@gmail.com>",
-    "version": "0.9.1",
-    "src": "https://raw.github.com/seajs/dew/master/build/es5-safe/es5-safe-debug.js",
-    "min": "https://raw.github.com/seajs/dew/master/build/es5-safe/es5-safe.js",
-    "gzipped": 1286
-  },
   "backbone": {
     "package": "https://raw.github.com/documentcloud/backbone/master/package.json",
     "src": "http://documentcloud.github.com/backbone/backbone.js",
@@ -38,11 +15,50 @@ define({
     "lib": ".",
     "main": "backbone.js",
     "version": "0.5.3",
-    "gzipped": 4944
+    "gzipped": "5KB",
+    "raw": "42KB"
+  },
+  "cookie": {
+    "package": "https://raw.github.com/seajs/dew/master/src/cookie/package.json",
+    "name": "cookie",
+    "description": "Provides utilities for dealing with cookies.",
+    "url": "https://github.com/seajs/dew/tree/master/src/cookie",
+    "keywords": [ "util" ],
+    "author": "Frank Wang <lifesinger@gmail.com>",
+    "version": "1.0.1",
+    "src": "https://raw.github.com/seajs/dew/master/build/cookie/cookie-debug.js",
+    "min": "https://raw.github.com/seajs/dew/master/build/cookie/cookie.js",
+    "gzipped": "1KB",
+    "raw": "5KB"
+  },
+  "es5-safe": {
+    "package": "https://raw.github.com/seajs/dew/master/src/es5-safe/package.json",
+    "name": "es5-safe",
+    "description": "Provides compatibility shims so that legacy JavaScript engines behave as closely as possible to ES5.",
+    "url": "https://github.com/seajs/dew/tree/master/src/es5-safe",
+    "keywords": [ "util" ],
+    "author": "Frank Wang <lifesinger@gmail.com>",
+    "version": "0.9.1",
+    "src": "https://raw.github.com/seajs/dew/master/build/es5-safe/es5-safe-debug.js",
+    "min": "https://raw.github.com/seajs/dew/master/build/es5-safe/es5-safe.js",
+    "gzipped": "1KB",
+    "raw": "10KB"
+  },
+  "jquery": {
+    "name": "jquery",
+    "description": "A new kind of JavaScript Library.",
+    "author": "John Resig",
+    "url": "http://jquery.com/",
+    "keywords": [ "dom", "event", "library" ],
+    "version": "1.6.4",
+    "src": "http://code.jquery.com/jquery-1.6.4.js",
+    "min": "http://code.jquery.com/jquery-1.6.4.min.js",
+    "gzipped": "31KB",
+    "raw": "233KB"
   },
   "json": {
     "package": "https://raw.github.com/seajs/dew/master/src/json/package.json",
-    "name": "JSON",
+    "name": "json",
     "description": "JSON in JavaScript",
     "url": "https://github.com/seajs/dew/tree/master/src/json",
     "keywords": [ "util" ],
@@ -50,11 +66,24 @@ define({
     "version": "1.0.0",
     "src": "https://raw.github.com/seajs/dew/master/build/json/json-debug.js",
     "min": "https://raw.github.com/seajs/dew/master/build/json/json.js",
-    "gzipped": 1303
+    "gzipped": "1KB",
+    "raw": "17KB"
+  },
+  "mustache": {
+    "package": "https://raw.github.com/janl/mustache.js/master/lib/package.json",
+    "version": "0.4.0",
+    "src": "https://raw.github.com/janl/mustache.js/master/mustache.js",
+    "name": "mustache",
+    "author": "http://mustache.github.com/",
+    "description": "{{ mustache }} in JavaScript — Logic-less templates.",
+    "keywords": [ "template" ],
+    "main": "./mustache",
+    "gzipped": "2KB",
+    "raw": "11KB"
   },
   "querystring": {
     "package": "https://raw.github.com/seajs/dew/master/src/querystring/package.json",
-    "name": "QueryString",
+    "name": "querystring",
     "description": "Provides utilities for dealing with query strings.",
     "url": "https://github.com/seajs/dew/tree/master/src/querystring",
     "keywords": [ "util" ],
@@ -62,7 +91,8 @@ define({
     "version": "1.0.0",
     "src": "https://raw.github.com/seajs/dew/master/build/querystring/querystring-debug.js",
     "min": "https://raw.github.com/seajs/dew/master/build/querystring/querystring.js",
-    "gzipped": 709
+    "gzipped": "1KB",
+    "raw": "5KB"
   },
   "seajs": {
     "package": "https://raw.github.com/seajs/seajs/master/package.json",
@@ -70,7 +100,7 @@ define({
     "src": "https://raw.github.com/seajs/seajs/master/build/sea-debug.js",
     "min": "https://raw.github.com/seajs/seajs/master/build/sea.js",
     "extra": "https://raw.github.com/seajs/seajs/master/build/plugin-map.js",
-    "name": "SeaJS",
+    "name": "seajs",
     "description": "SeaJS is a new kind of JavaScript Loader that makes it easy to build scalable web applications. SeaJS is designed to change the way you organize JavaScript.",
     "homepage": "https://seajs.com/",
     "keywords": [ "loader", "modular", "CommonJS", "browser", "framework" ],
@@ -83,7 +113,8 @@ define({
     },
     "main": "build/sea.js",
     "version": "1.0.2",
-    "gzipped": 3834
+    "gzipped": "4KB",
+    "raw": "30KB"
   },
   "underscore": {
     "package": "https://raw.github.com/documentcloud/underscore/master/package.json",
@@ -102,6 +133,7 @@ define({
     },
     "main": "underscore.js",
     "version": "1.2.0",
-    "gzipped": 3963
+    "gzipped": "4KB",
+    "raw": "32KB"
   }
 });
