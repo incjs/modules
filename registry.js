6805e923a9e4c180e059c8c4f9aa36dc5ba1d0b1
lifesinger <lifesinger@gmail.com>
2012-04-12 18:58:39 +0800

    add jasmine module

--- registry.js
+++ registry.js
@@ -308,5 +308,16 @@ define({
     "dirpath": "zepto",
     "gzipped": "9KB",
     "raw": "51KB"
+  },
+  "jasmine": {
+    "name": "jasmine",
+    "description": "Jasmine is a behavior-driven development framework for testing your JavaScript code. It does not depend on any other JavaScript frameworks. It does not require a DOM. And it has a clean, obvious syntax so that you can easily write tests.",
+    "author": "Pivotal Labs",
+    "url": "http://pivotal.github.com/jasmine/",
+    "keywords": "test",
+    "version": "1.1.0",
+    "filename": "jasmine",
+    "dirpath": "jasmine",
+    "gzipped": "15KB"
   }
 });
