c0e466ad7bd52c1d36e199035e918fda7a12fc67
popomore <sakura9515@gmail.com>
2012-09-11 21:24:50 +0800

    update jasmine-jquery

--- jasmine-jquery/1.4.2/jasmine-jquery-debug.js
+++ jasmine-jquery/1.4.2/jasmine-jquery-debug.js
@@ -551,4 +551,20 @@ afterEach(function() {
 })
 
 
+  window.readFixtures = readFixtures;
+  window.preloadFixtures = preloadFixtures;
+  window.loadFixtures = loadFixtures;
+  window.appendLoadFixtures = appendLoadFixtures;
+  window.setFixtures = setFixtures;
+  window.appendSetFixtures = appendSetFixtures;
+  window.sandbox = sandbox;
+  window.spyOnEvent = spyOnEvent;
+  window.preloadStyleFixtures = preloadStyleFixtures;
+  window.loadStyleFixtures = loadStyleFixtures;
+  window.appendLoadStyleFixtures = appendLoadStyleFixtures;
+  window.setStyleFixtures = setStyleFixtures;
+  window.appendSetStyleFixtures = appendSetStyleFixtures;
+  window.loadJSONFixtures = loadJSONFixtures;
+  window.getJSONFixture = getJSONFixture;
+
 });
