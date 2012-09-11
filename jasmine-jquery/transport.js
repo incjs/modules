ee023441095c077aef7aa4aa415905b451afd948
popomore <sakura9515@gmail.com>
2012-09-11 19:33:34 +0800

    add jasmine-jquery

---
+++ jasmine-jquery/transport.js
@@ -0,0 +1,13 @@
+/**
+ * @name jasmine-jquery
+ *
+ * @src https://raw.github.com/velesin/jasmine-jquery/master/lib/jasmine-jquery.js
+ */
+
+define('#{{id}}', ['$'], function(require) {
+
+  var jQuery = require('$');
+
+  /*{{code}}*/
+
+});
