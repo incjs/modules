9c8d7da0672bf2604e9acfeb08ed17b286f02eda
popomore <sakura9515@gmail.com>
2012-09-13 19:49:01 +0800

    add jquery-color

--- jquery-color/transport.js
+++ jquery-color/transport.js
@@ -1,6 +1,6 @@
 /**
  * @package https://raw.github.com/jquery/jquery-color/master/package.json
- * @src https://raw.github.com/jquery/jquery-color/{{version}}/jquery-color.js
+ * @src https://raw.github.com/jquery/jquery-color/{{version}}/jquery.color.js
  */
 
 define('#{{id}}', ['$'], function(require, exports, module) {
