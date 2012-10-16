e11681db204beb4b1def4de1691b6c7a554da9c8
popomore <sakura9515@gmail.com>
2012-10-16 11:07:56 +0800

    add keymaster

---
+++ keymaster/transport.js
@@ -0,0 +1,12 @@
+/**
+ * @package https://raw.github.com/madrobby/keymaster/master/package.json
+ * @src https://raw.github.com/madrobby/keymaster/master/keymaster.js
+ */
+
+define('#{{id}}', [], function(require, exports, module) {
+  var keymaster;
+  (function() {
+    /*{{code}}*/
+  }).call(keymaster);
+});
+
