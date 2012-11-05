33eeae6c446b064b6e0165d660e3ce9069cc9457
kangpangpang <kangpangpang@gmail.com>
2012-11-05 14:19:40 +0800

    update modules

---
+++ switchable/0.9.11/tabs-debug.js
@@ -0,0 +1,7 @@
+define("#switchable/0.9.11/tabs-debug", ["./switchable-debug", "$-debug", "#easing/1.0.0/easing-debug", "#widget/1.0.2/widget-debug", "#base/1.0.1/base-debug", "#class/1.0.0/class-debug", "#events/1.0.0/events-debug"], function(require, exports, module) {
+
+    var Switchable = require('./switchable-debug');
+
+    // 展现型标签页组件
+    module.exports = Switchable.extend({});
+});
