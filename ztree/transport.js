0092d95551eb4a612255f37d629364e768a6a492
Denis <swainet@126.com>
2012-11-08 20:07:35 +0800

    add ztree

---
+++ ztree/transport.js
@@ -0,0 +1,16 @@
+/**
+ * @name zTree
+ * @description A jQuery tree plugin.
+ * @author zTree
+ * @url http://ztree.me/
+ * @keywords tree
+ *
+ * @src https://github.com/swain/zTree/blob/master/3.4/ztree-debug.js
+ * @min https://github.com/swain/zTree/blob/master/3.4/ztree.js
+ * @version 3.4
+ */
+
+define('#{{id}}', ['$'], function(require, exports, module) {
+  var jQuery = require('$');
+  /*{{code}}*/
+});
